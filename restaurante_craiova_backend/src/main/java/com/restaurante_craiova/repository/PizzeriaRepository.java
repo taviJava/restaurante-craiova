af2b0c8de809db0d40a390b6dc1255eb51d9f797
lrusanescu <lrusanescu@yahoo.com>
2020-12-15 20:25:11 +0200

    Clasele repository

---
+++ restaurante_craiova_backend/src/main/java/com/restaurante_craiova/repository/PizzeriaRepository.java
@@ -0,0 +1,10 @@
+package com.restaurante_craiova.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.Entity;
+
+@Repository
+public interface PizzeriaRepository  extends JpaRepository {
+}
