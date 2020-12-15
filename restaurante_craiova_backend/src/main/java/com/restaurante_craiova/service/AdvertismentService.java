8bebbc39a1edda56992aa1cc0e141225eb819c0b
lrusanescu <lrusanescu@yahoo.com>
2020-12-15 22:05:03 +0200

    service

--- restaurante_craiova_backend/src/main/java/com/restaurante_craiova/service/AdvertismentService.java
+++ restaurante_craiova_backend/src/main/java/com/restaurante_craiova/service/AdvertismentService.java
@@ -1,7 +1,74 @@
 package com.restaurante_craiova.service;
 
+import com.restaurante_craiova.persistance.dto.AdvertisementDto;
+import com.restaurante_craiova.persistance.model.AdvertisementModel;
+import com.restaurante_craiova.repository.AdvertisementRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AdvertismentService {
+    @Autowired
+    AdvertisementRepository advertisementRepository;
+
+    public void save(AdvertisementDto advertisementDto) {
+        AdvertisementModel advertisementModel = new AdvertisementModel();
+        advertisementRepository.save(getModel(advertisementModel, advertisementDto));
+    }
+
+    public List<AdvertisementDto> getAll() {
+        List<AdvertisementModel> advertisementModelList = advertisementRepository.findAll();
+        List<AdvertisementDto> advertisementDtoList = new ArrayList<>();
+        for (AdvertisementModel advertisementModel : advertisementModelList) {
+            AdvertisementDto advertisementDto = new AdvertisementDto();
+            advertisementDtoList.add(getDto(advertisementDto,advertisementModel));
+        }
+        return advertisementDtoList;
+    }
+
+    public AdvertisementDto findById(long id) {
+        Optional<AdvertisementModel> advertisementModelOptional = advertisementRepository.findById(id);
+        AdvertisementDto advertisementDto = new AdvertisementDto();
+        if (advertisementModelOptional.isPresent()) {
+            AdvertisementModel advertisementModel = advertisementModelOptional.get();
+            getDto(advertisementDto, advertisementModel);
+        }
+        return advertisementDto;
+    }
+
+    public void update(AdvertisementDto advertisementDto) {
+        Optional<AdvertisementModel> advertisementModelOptional = advertisementRepository.findById(advertisementDto.getId());
+        if (advertisementModelOptional.isPresent()) {
+            AdvertisementModel advertisementModel = advertisementModelOptional.get();
+            advertisementRepository.save(getModel(advertisementModel, advertisementDto));
+        }
+    }
+
+    public void delete(long id) {
+        advertisementRepository.deleteById(id);
+    }
+
+    private AdvertisementModel getModel(AdvertisementModel advertisementModel, AdvertisementDto advertisementDto) {
+        advertisementModel.setFirstName(advertisementDto.getFirstName());
+        advertisementModel.setLastName(advertisementDto.getLastName());
+        advertisementModel.setMail(advertisementDto.getMail());
+        advertisementModel.setPhone(advertisementDto.getPhone());
+        advertisementModel.setTextMessage(advertisementDto.getTextMessage());
+        return advertisementModel;
+    }
+
+    private AdvertisementDto getDto(AdvertisementDto advertisementDto, AdvertisementModel advertisementModel) {
+        advertisementDto.setFirstName(advertisementModel.getFirstName());
+        advertisementDto.setLastName(advertisementModel.getLastName());
+        advertisementDto.setMail(advertisementModel.getMail());
+        advertisementDto.setPhone(advertisementModel.getPhone());
+        advertisementDto.setTextMessage(advertisementModel.getTextMessage());
+        advertisementDto.setId(advertisementModel.getId());
+        return advertisementDto;
+    }
 }
--- restaurante_craiova_backend/src/main/java/com/restaurante_craiova/service/EvenimentService.java
+++ restaurante_craiova_backend/src/main/java/com/restaurante_craiova/service/EvenimentService.java
@@ -1,7 +1,87 @@
 package com.restaurante_craiova.service;
 
+import com.restaurante_craiova.persistance.dto.EvenimentDto;
+import com.restaurante_craiova.persistance.model.EvenimentModel;
+import com.restaurante_craiova.persistance.model.EvenimentType;
+import com.restaurante_craiova.repository.EvenimentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class EvenimentService {
+    @Autowired
+    private EvenimentRepository evenimentRepository;
+
+    public void save(EvenimentDto evenimentDto) {
+        EvenimentModel evenimentModel = new EvenimentModel();
+        evenimentModel.setId(evenimentDto.getId());
+        evenimentModel.setName(evenimentDto.getName());
+        evenimentModel.setEmail(evenimentDto.getEmail());
+        evenimentModel.setDate(evenimentDto.getDate());
+        evenimentModel.setPhone(evenimentDto.getPhone());
+        evenimentModel.setTextMessage(evenimentDto.getTextMessage());
+        evenimentModel.setNumberOfPersons(evenimentDto.getNumberOfPersons());
+        // evenimentModel.setZone(evenimentDto.getZone());
+        evenimentRepository.save(evenimentModel);
+    }
+
+    public List<EvenimentDto> getAll() {
+        List<EvenimentModel> evenimentModelList = evenimentRepository.findAll();
+        List<EvenimentDto> evenimentDtoList = new ArrayList<>();
+        for (EvenimentModel evenimentModel : evenimentModelList) {
+            EvenimentDto evenimentDto = new EvenimentDto();
+            evenimentDto.setId(evenimentModel.getId());
+            evenimentDto.setName(evenimentModel.getName());
+            evenimentDto.setEmail(evenimentModel.getEmail());
+            evenimentDto.setDate(evenimentDto.getDate());
+            evenimentDto.setNumberOfPersons(evenimentDto.getNumberOfPersons());
+            evenimentDto.setPhone(evenimentModel.getPhone());
+            evenimentDto.setTextMessage(evenimentModel.getTextMessage());
+            evenimentDto.setEvenimentType(evenimentModel.getEvenimentType().name());
+            evenimentDto.setZone(evenimentModel.getZone().name());
+            evenimentDtoList.add(evenimentDto);
+        }
+        return evenimentDtoList;
+    }
+
+    public EvenimentDto findById(long id) {
+        Optional<EvenimentModel> evenimentModelOptional = evenimentRepository.findById(id);
+        EvenimentDto evenimentDto = new EvenimentDto();
+        if (evenimentModelOptional.isPresent()) {
+            EvenimentModel evenimentModel = evenimentModelOptional.get();
+            evenimentDto.setId(evenimentModel.getId());
+            evenimentDto.setName(evenimentModel.getName());
+            evenimentDto.setEmail(evenimentModel.getEmail());
+            evenimentDto.setDate(evenimentDto.getDate());
+            evenimentDto.setNumberOfPersons(evenimentDto.getNumberOfPersons());
+            evenimentDto.setPhone(evenimentModel.getPhone());
+            evenimentDto.setTextMessage(evenimentModel.getTextMessage());
+            evenimentDto.setEvenimentType(evenimentModel.getEvenimentType().name());
+            evenimentDto.setZone(evenimentModel.getZone().name());
+        }
+        return evenimentDto;
+    }
+
+    public void update(EvenimentDto evenimentDto) {
+        Optional<EvenimentModel> evenimentModelOptional = evenimentRepository.findById(evenimentDto.getId());
+        if (evenimentModelOptional.isPresent()) {
+            EvenimentModel evenimentModel = evenimentModelOptional.get();
+            evenimentModel.setId(evenimentDto.getId());
+            evenimentModel.setName(evenimentDto.getName());
+            evenimentModel.setEmail(evenimentDto.getEmail());
+            evenimentModel.setDate(evenimentDto.getDate());
+            evenimentModel.setPhone(evenimentDto.getPhone());
+            evenimentModel.setTextMessage(evenimentDto.getTextMessage());
+            evenimentModel.setNumberOfPersons(evenimentDto.getNumberOfPersons());
+            evenimentRepository.save(evenimentModel);
+        }
+    }
+
+    public void delete(long id) {
+        evenimentRepository.deleteById(id);
+    }
 }
