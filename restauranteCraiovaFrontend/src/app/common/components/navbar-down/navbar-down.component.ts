841a79829c3dc1e4479aee8c955b6fdfa67a1a2c
taviJava <tavi.zorila@gmail.com>
2020-12-20 17:26:28 +0200

    nav down

---
+++ restauranteCraiovaFrontend/src/app/common/components/navbar-down/navbar-down.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-navbar-down',
+  templateUrl: './navbar-down.component.html',
+  styleUrls: ['./navbar-down.component.css']
+})
+export class NavbarDownComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
