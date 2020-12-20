f13acc39cd747392a0c8327af8b21757e732ab9f
taviJava <tavi.zorila@gmail.com>
2020-12-20 16:06:55 +0200

    modificari la nav-bar

--- restauranteCraiovaFrontend/src/app/common/components/navbar-up/navbar-up.component.ts
+++ restauranteCraiovaFrontend/src/app/common/components/navbar-up/navbar-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-navbar-up',
@@ -7,9 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+// tslint:disable-next-line:typedef
+goToHome(){
+    this.router.navigate(['']);
+}
 }
