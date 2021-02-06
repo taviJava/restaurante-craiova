7f8c5f8d894e9ee967e9b957425b8ebaccd1c97c
taviJava <tavi.zorila@gmail.com>
2021-02-06 19:47:22 +0200

    no message

--- restauranteCraiovaFrontend/src/app/test/components/test2/test2.component.ts
+++ restauranteCraiovaFrontend/src/app/test/components/test2/test2.component.ts
@@ -1,38 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import 'ol/ol.css';
-import BingMaps from 'ol/source/BingMaps';
 import Map from 'ol/Map';
-import TileLayer from 'ol/layer/Tile';
+import VectorSource from 'ol/source/Vector';
+import VectorLayer from 'ol/layer/Vector';
 import View from 'ol/View';
+import Feature from 'ol/Feature';
+import Point from 'ol/geom/Point';
+import XyzSource from 'ol/source/XYZ';
+import TileLayer from 'ol/layer/Tile';
+import Style from 'ol/style/Style';
+import Icon from 'ol/style/Icon';
+
+import { fromLonLat } from 'ol/proj';
 @Component({
   selector: 'app-test2',
   templateUrl: './test2.component.html',
   styleUrls: ['./test2.component.css']
 })
 export class Test2Component implements OnInit {
-  styles = [
-    'RoadOnDemand',
-    'Aerial',
-    'AerialWithLabelsOnDemand',
-    'CanvasDark',
-    'OrdnanceSurvey' ];
-   layers = [];
- map: any;
- select = document.getElementById('layer-select');
+  map: Map;
+  vectorSource: VectorSource;
+  vectorLayer: VectorLayer;
+  xyzSource: XyzSource;
+  tileLayer: TileLayer;
+  view: View;
+  marker: Feature;
+  iconStyle = new Style({
+    image: new Icon({
+      anchor: [0.5, 0.5],
+      anchorXUnits: 'fraction',
+      anchorYUnits: 'fraction',
+      src: 'assets/map/mapSymbol.PNG',
+    }),
+  });
   constructor() { }
 
   ngOnInit(): void {
+    // Feature and vector
+    this.marker = new Feature({
+      geometry: new Point(fromLonLat([ 23.76040 , 44.33405]))
+    });
+    this.marker.setStyle(this.iconStyle);
+
+    this.vectorSource = new VectorSource({
+      features: [this.marker]
+    });
+
+    this.vectorLayer = new VectorLayer({
+      source: this.vectorSource
+    });
+
+    // XYZ
+    this.xyzSource = new XyzSource({
+      url: 'http://tile.osm.org/{z}/{x}/{y}.png'
+    });
+
+    this.tileLayer = new TileLayer({
+      source: this.xyzSource
+    });
+
+    // View and map
+    this.view = new View({
+      center: fromLonLat([23.76040, 44.33405]),
+      zoom: 14
+    });
+
     this.map = new Map({
-      layers: [
-        new TileLayer({
-          target: 'map'
-        })
-      ],
-      view: new View({
-        center: [-6655.5402445057125, 6709968.258934638],
-        zoom: 13,
-      })
+      target: 'map',
+      layers: [this.tileLayer, this.vectorLayer],
+      view: this.view
     });
   }
+  }
 
-}
+
